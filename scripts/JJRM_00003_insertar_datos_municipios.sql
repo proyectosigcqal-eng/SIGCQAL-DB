636d25fa1fdea8185ba49cfc7c1c38600938a229
JuanMerino15 <juanrivmerino4@gmail.com>
2026-04-06 17:33:31 -0600

    feat: agregar catalogos estados y municipios

---
+++ scripts/JJRM_00003_insertar_datos_municipios.sql
@@ -0,0 +1,64 @@
+INSERT INTO public.cat_municipios (id_estado,nombre_municipio) VALUES
+	 (32,'Apozol'),
+	 (32,'Apulco'),
+	 (32,'Atolinga'),
+	 (32,'Benito Juárez'),
+	 (32,'Calera'),
+	 (32,'Cañitas de Felipe Pescador'),
+	 (32,'Concepción del Oro'),
+	 (32,'Cuauhtémoc'),
+	 (32,'Chalchihuites'),
+	 (32,'Fresnillo');
+INSERT INTO public.cat_municipios (id_estado,nombre_municipio) VALUES
+	 (32,'Trinidad García de la Cadena'),
+	 (32,'Genaro Codina'),
+	 (32,'General Enrique Estrada'),
+	 (32,'General Francisco R. Murguía'),
+	 (32,'El Plateado de Joaquín Amaro'),
+	 (32,'General Pánfilo Natera'),
+	 (32,'Guadalupe'),
+	 (32,'Huanusco'),
+	 (32,'Jalpa'),
+	 (32,'Jerez');
+INSERT INTO public.cat_municipios (id_estado,nombre_municipio) VALUES
+	 (32,'Jiménez del Teul'),
+	 (32,'Juan Aldama'),
+	 (32,'Juchipila'),
+	 (32,'Loreto'),
+	 (32,'Luis Moya'),
+	 (32,'Mazapil'),
+	 (32,'Melchor Ocampo'),
+	 (32,'Mezquital del Oro'),
+	 (32,'Miguel Auza'),
+	 (32,'Momax');
+INSERT INTO public.cat_municipios (id_estado,nombre_municipio) VALUES
+	 (32,'Monte Escobedo'),
+	 (32,'Morelos'),
+	 (32,'Moyahua de Estrada'),
+	 (32,'Nochistlán de Mejía'),
+	 (32,'Noria de Ángeles'),
+	 (32,'Ojocaliente'),
+	 (32,'Pánuco'),
+	 (32,'Pinos'),
+	 (32,'Río Grande'),
+	 (32,'Sain Alto');
+INSERT INTO public.cat_municipios (id_estado,nombre_municipio) VALUES
+	 (32,'El Salvador'),
+	 (32,'Sombrerete'),
+	 (32,'Susticacán'),
+	 (32,'Tabasco'),
+	 (32,'Tepechitlán'),
+	 (32,'Tepetongo'),
+	 (32,'Teul de González Ortega'),
+	 (32,'Tlaltenango de Sánchez Román'),
+	 (32,'Valparaíso'),
+	 (32,'Vetagrande');
+INSERT INTO public.cat_municipios (id_estado,nombre_municipio) VALUES
+	 (32,'Villa de Cos'),
+	 (32,'Villa García'),
+	 (32,'Villa González Ortega'),
+	 (32,'Villa Hidalgo'),
+	 (32,'Villanueva'),
+	 (32,'Zacatecas'),
+	 (32,'Trancoso'),
+	 (32,'Santa María de la Paz');
