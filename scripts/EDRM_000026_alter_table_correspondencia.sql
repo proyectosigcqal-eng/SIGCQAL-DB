bb5e4786fffb23f656fc91b2703e861753b470c6
Erick Rivera <erick.rivemora@gmail.com>
2026-05-07 14:52:39 -0600

    cambios v2

--- scripts/EDRM_000026_alter_table_correspondencia.sql
+++ scripts/EDRM_000026_alter_table_correspondencia.sql
@@ -1,4 +1,4 @@
 --liquibase formatted sql
---changeset Erick Rivera: modificar la tabla  correspondencia en esquema correspondencia_VFinal´
+--changeset Erick Rivera: modificar la tabla  correspondencia en esquema correspondencia_VFinal
 -- Agregamos la columna
 ALTER TABLE correspondencia.correspondencia ADD COLUMN id_tipo_correspondencia INT4;
--- scripts/EDRM_000028_insert_table_tipo_correspondencia.sql
+++ scripts/EDRM_000028_insert_table_tipo_correspondencia.sql
@@ -1,6 +1,6 @@
 --liquibase formatted sql
 --changeset Erick Rivera: insertar datos tabla tipo correspondencia en esquema catalogos_vfinal
 -- Insertar los tipos de correspondencia solicitados
-INSERT INTO correspondencia.tipo_correspondencia (id_tipo_correspondencia, descripcion) VALUES
+INSERT INTO catalogos.tipo_correspondencia (id_tipo_correspondencia, descripcion) VALUES
     (DEFAULT, 'externa'),
     (DEFAULT, 'interna');
