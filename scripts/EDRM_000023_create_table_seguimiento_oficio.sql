1c0be508c11a4c5fe1bf54b403416ba9abd6c632
Erick Rivera <erick.rivemora@gmail.com>
2026-05-07 13:54:47 -0600

    cambios del --changeset Erick Rivera:creacion_seguimiento_oficio_v2 splitStatements:false
    camvios --changeset Erick Rivera:creacion_seguimiento_oficio_v2 splitStatements:false

--- scripts/EDRM_000023_create_table_seguimiento_oficio.sql
+++ scripts/EDRM_000023_create_table_seguimiento_oficio.sql
@@ -1,21 +1,16 @@
 --liquibase formatted sql
---changeset Erick Rivera: crear_tabla_seguimiento_acuse_vFINAL
-CREATE TABLE correspondencia.seguimiento_oficio (
-    -- ID autoincremental (SERIAL4)
+--changeset Erick Rivera:creacion_seguimiento_oficio_v2 splitStatements:false
+--comment: Creacion de tabla seguimiento_oficio con proteccion de existencia
+
+CREATE TABLE IF NOT EXISTS correspondencia.seguimiento_oficio (
     id_seguimiento_oficio SERIAL4 PRIMARY KEY,
-    
-    -- ID Natural (Cambiado de id_memo a id_oficio)
     id_oficio INT4,
-    
-    -- Folio de respuesta también es SERIAL4 en la imagen
     folio_respuesta SERIAL4,
-    
-    -- Resto de columnas respetando tipos de datos
     respuesta_seguimiento_oficio TEXT,
     fecha_resolucion DATE,
     hora_resolucion TIME,
     archivo_adjunto VARCHAR(255),
     id_usuario INT4,
     id_estatus INT4,
-    fecha_registro TIMESTAMP
+    fecha_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
---
+++ scripts/EDRM_000032_create_table_oficio_contestacion_externa.sql
@@ -0,0 +1,35 @@
+--liquibase formatted sql
+--changeset Erick Rivera:crear_tabla_contestacion_oficio_externo_vFINAL
+CREATE TABLE correspondencia.oficio_contestacion_externa (
+    -- PK propia de la tabla
+    id_oficio_contestacion serial4 NOT NULL,
+    
+    -- FK hacia la tabla que me mostraste (Correspondencia original)
+    id_correspondencia int4 NOT NULL,
+    
+    -- FK hacia el usuario que crea el oficio (El Encargado)
+    id_usuario_emisor int4 NOT NULL,
+    
+    -- Datos del Oficio de Salida
+    num_oficio_salida varchar(100) NOT NULL, -- Ej: CEDZ/CONTESTACION/2024/005
+    asunto_contestacion text NOT NULL,
+    cuerpo_oficio_texto text NOT NULL, -- El contenido redactado
+    
+    -- Gestión de Archivo
+    url_pdf_final varchar(255), -- Ruta del documento generado
+    
+    -- Tiempos
+    fecha_emision timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    -- Restricciones
+    CONSTRAINT pk_oficio_contestacion PRIMARY KEY (id_oficio_contestacion),
+    
+    CONSTRAINT fk_contestacion_correspondencia 
+        FOREIGN KEY (id_correspondencia) 
+        REFERENCES correspondencia.correspondencia (id_correspondencia)
+        ON DELETE CASCADE,
+        
+    CONSTRAINT fk_contestacion_usuario 
+        FOREIGN KEY (id_usuario_emisor) 
+        REFERENCES catalogos.usuarios (id_usuario) 
+);
