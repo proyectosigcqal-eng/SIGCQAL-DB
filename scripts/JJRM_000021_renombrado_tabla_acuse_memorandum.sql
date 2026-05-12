04ea27d577813e9413e72154d55b2696cee04295
JuanMerino15 <juanrivmerino4@gmail.com>
2026-05-11 20:40:42 -0600

    .

--- scripts/JJRM_000021_renombrado_tabla_acuse_memorandum.sql
+++ scripts/JJRM_000021_renombrado_tabla_acuse_memorandum.sql
@@ -1,5 +1,8 @@
 --liquibase formatted sql
 --changeset JoseJuanRiveraMerino:renombrado_forzado_v2
+--preconditions onFail:MARK_RAN
+--precondition-sql-check expectedResult:1 SELECT count(*) FROM information_schema.tables WHERE table_schema='correspondencia' AND table_name='acuserecibointerno';
 
+-- Renombramos la tabla
 ALTER TABLE correspondencia.acuserecibointerno RENAME TO acuse_memorandum;
 --rollback ALTER TABLE correspondencia.acuse_memorandum RENAME TO acuserecibointerno;
