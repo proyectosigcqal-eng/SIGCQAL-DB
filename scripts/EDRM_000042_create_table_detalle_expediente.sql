cd712417cf3442ca7febb69c84608738b5c5da9d
Erick Rivera <erick.rivemora@gmail.com>
2026-05-26 11:16:56 -0600

    correccion. agregar tabla detalle_asesoria

--- scripts/EDRM_000042_create_table_detalle_expediente.sql
+++ scripts/EDRM_000042_create_table_detalle_expediente.sql
@@ -1,33 +1,30 @@
 --liquibase formatted sql
 
---changeset ErickRivera:crear-tabla-expedientes dbms:postgresql
---comment: Creacion de la tabla maestra de expedientes con herencia e integridad referencial ajustada.
+--changeset ErickRivera:crear-tabla-detalle_asesoria dbms:postgresql
+--comment: Creacion de la tabla detalle asesoria.
 
-CREATE TABLE IF NOT EXISTS sustantiva.expedientes (
-    id_expediente SERIAL PRIMARY KEY,
-    folio_gobierno VARCHAR(30) UNIQUE NOT NULL,
-    fecha_solicitud TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    id_municipio INT NOT NULL,
-    id_asesor INT NOT NULL,
-    id_contribuyente INT NOT NULL,
-    id_solicitante INT NOT NULL,
-    id_tipo_tramite INT NOT NULL,
-    id_estatus_expediente INT NOT NULL,
-    documento_acredita_personalidad VARCHAR(255),
-    archivo_documento_acredita_personalidad VARCHAR(255),
+CREATE TABLE IF NOT EXISTS sustantiva.detalle_asesoria (
+    id_detalle_asesoria SERIAL PRIMARY KEY,
+    id_expediente INT NOT NULL UNIQUE, -- UNIQUE garantiza relación 1 a 1 con el expediente padre
+    id_tipo_acto_emitido INT NOT NULL,
+    id_autoridad INT NOT NULL,
+    calificacion_acto TEXT,
+    id_tipo_entrada INT NOT NULL,
+    problematica TEXT,
+    seguimiento TEXT,
+    id_estatus_detalle_expediente INT NOT NULL,
 
-    CONSTRAINT fk_exp_municipio 
-        FOREIGN KEY (id_municipio) REFERENCES catalogos.cat_municipios (id_municipio),
-    CONSTRAINT fk_exp_asesor 
-        FOREIGN KEY (id_asesor) REFERENCES sustantiva.asesores(id_asesores),
-    CONSTRAINT fk_exp_contribuyente 
-        FOREIGN KEY (id_contribuyente) REFERENCES sustantiva.contribuyentes(id_contribuyentes),
-    CONSTRAINT fk_exp_solicitante 
-        FOREIGN KEY (id_solicitante) REFERENCES catalogos.personas(id_persona),
-    CONSTRAINT fk_exp_tipo_tramite 
-        FOREIGN KEY (id_tipo_tramite) REFERENCES catalogos.tipo_tramite(id_tipo_tramite),
-    CONSTRAINT fk_exp_estatus 
-        FOREIGN KEY (id_estatus_expediente) REFERENCES catalogos.estatus_expediente(id_estatus_expediente)
+    -- Restricciones de Llaves Foráneas (FK)
+    CONSTRAINT fk_det_expediente 
+        FOREIGN KEY (id_expediente) REFERENCES sustantiva.expedientes(id_expediente) ON DELETE CASCADE,
+    CONSTRAINT fk_det_tipo_acto 
+        FOREIGN KEY (id_tipo_acto_emitido) REFERENCES catalogos.tipo_acto_emitido(id_tipo_acto_emitido),
+    CONSTRAINT fk_det_autoridad 
+        FOREIGN KEY (id_autoridad) REFERENCES catalogos.autoridades(id_autoridad),
+    CONSTRAINT fk_det_tipo_entrada 
+        FOREIGN KEY (id_tipo_entrada) REFERENCES catalogos.tipo_entrada(id_tipo_entrada),
+    CONSTRAINT fk_det_estatus_detalle 
+        FOREIGN KEY (id_estatus_detalle_expediente) REFERENCES catalogos.estatus_detalle_expediente(id_estatus_detalle_expediente)
 );
 
---rollback DROP TABLE sustantiva.expedientes;
+--rollback DROP TABLE sustantiva.detalle_asesoria;
