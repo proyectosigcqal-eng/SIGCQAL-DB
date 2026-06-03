3d39dff6131e4db4e615d8a18cc7142f3b81ce4b
rodrivmac1 <166084548+rodrivmac1@users.noreply.github.com>
2026-06-02 23:34:35 -0600

    Create RRM_00044_alter_table_expediente.sql

---
+++ scripts/RRM_00044_alter_table_expediente.sql
@@ -0,0 +1,14 @@
+--liquibase formatted sql
+
+--changeset RodrigoRivera:alter-tabla-expedientes dbms:postgresql
+--comment: Alteracion de la tabla expedientes.
+ALTER TABLE sustantiva.expedientes 
+    ADD COLUMN id_representante_legal INT;
+
+ALTER TABLE sustantiva.expedientes
+    ADD CONSTRAINT fk_expedientes_representante_legal
+    FOREIGN KEY (id_representante_legal) 
+    REFERENCES catalogos.personas (id_persona);
+
+--rollback ALTER TABLE sustantiva.expedientes DROP CONSTRAINT IF EXISTS fk_expedientes_representante_legal;
+--rollback ALTER TABLE sustantiva.expedientes DROP COLUMN IF EXISTS id_representante_legal;
